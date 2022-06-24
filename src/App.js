35f810202d560040862b9f828a9ebfb1ec2eb508
rawwr <akshayace0302@gmail.com>
2022-06-25 00:33:52 +0530

    handling http errors

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import "./App.module.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
 
   const fetchMovieHandler = async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      };
-    });
-    // console.log(transformedMovies);
-    setMovies(transformedMovies);
+    setIsError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Lmao, we have messed up our backend (Comeback later)");
+      }
+      const data = await response.json();
+      //   console.log(data);
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      // console.log(transformedMovies);
+      setMovies(transformedMovies);
+    } catch (error) {
+      setIsError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -30,9 +42,10 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && (
+        {!isLoading && !isError && movies.length === 0 && (
           <h1>Please Fetch To see The MoviesList</h1>
         )}
+        {!isLoading && isError && <h1>{isError}</h1>}
         {isLoading && <h1>We are loading! HangOn there</h1>}
       </section>
     </React.Fragment>
